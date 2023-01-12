d1f06c922b6c4d79a08a8c59959757bf9fb3e333
Henrique Oliveira Marques <oli@sdu.dk>
2023-01-12 01:41:28 +0100

    Update libsvdd.m

--- Algorithms/libsvdd.m
+++ Algorithms/libsvdd.m
@@ -19,11 +19,6 @@
 % be rejected, when supplied FRACERR gives (an upper bound) for the
 % fraction of data which is completely outside the description.
 %
-% Note: this version of the SVDD is not compatible with older dd_tools
-% versions. This is to make the use of consistent_occ.m possible.
-%
-% Further note: this classifier is one of the few which can actually
-% deal with example outlier objects!
 % 
 % REFERENCE
 %@article{Tax1999c,
@@ -34,14 +29,7 @@
 %	number = {11-13},pages = {1191-1199}
 %}
 %
-% SEE ALSO
-% incsvdd, svdd_optrbf, dd_roc.
-
-% Copyright: D.M.J. Tax, D.M.J.Tax@prtools.org
-% Faculty EWI, Delft University of Technology
-% P.O. Box 5031, 2600 GA Delft, The Netherlands
   
-%function W = svdd(a,fracrej,sigma)
 function W = svdd(varargin)
 
 argin = shiftargin(varargin,'scalar');
@@ -54,31 +42,32 @@ elseif mapping_task(argin,'training')
    [a,fracrej,sigma] = deal(argin{:});
    a = target_class(a);
 
-	c = 1/(size(a, 1)*fracrej(1));
-	s = 1/(sigma^2);
-	model = svmtrain(a.nlab, +a, ['-s 5 -t 2 -g ', num2str(s), ' -c ', num2str(c), ' -e 1e-16 -q ']);
+   c = 1/(size(a, 1)*fracrej(1));
+   s = 1/(sigma^2);
+   model = svmtrain(a.nlab, +a, ['-s 5 -t 2 -g ', num2str(s), ' -c ', num2str(c), ' -e 1e-16 -q ']);
 	
-	% store the results
-	W.model = model;
-    [~, ~, W.out] = svmpredict(repmat(0, size(a,1), 1), +a, W.model, '-q');
-	W.s = sigma;
-	W.threshold = 0;
-	W = prmapping(mfilename,'trained',W,char('target','outlier'),size(a,2),2);
-	W = setname(W,'SVDD');
+   % store the results
+   W.model = model;
+   [~, ~, W.out] = svmpredict(repmat(0, size(a,1), 1), +a, W.model, '-q');
+   W.s = sigma;
+   W.threshold = 0;
+   W = prmapping(mfilename,'trained',W,char('target','outlier'),size(a,2),2);
+   W = setname(W,'SVDD');
+
 elseif mapping_task(argin,'trained execution') %testing
 
    [a,fracrej] = deal(argin{1:2});
-	W = getdata(fracrej);
-	m = size(a,1);
+   W = getdata(fracrej);
+   m = size(a,1);
 
-	% check if alpha's are OK
-	[~, ~, out] = svmpredict(repmat(0, m, 1), +a, W.model, '-q');
-    
-    newout = [out repmat(W.threshold,m,1)];
+   [~, ~, out] = svmpredict(repmat(0, m, 1), +a, W.model, '-q');
+   
+   newout = [out repmat(W.threshold,m,1)];
 
-	% Store the distance as output:
-	W = setdat(a,-newout,fracrej);
-	W = setfeatdom(W,{[-inf inf; -inf inf] [-inf inf; -inf inf]});
+   % Store the distance as output:
+   W = setdat(a,-newout,fracrej);
+   W = setfeatdom(W,{[-inf inf; -inf inf] [-inf inf; -inf inf]});
+   
 else
    error('Illegal call to SVDD.');
 end
